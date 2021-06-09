934f60ae1c249d7b43d27c60cd2844b48b96d91c
reelyizi <batuhan.eyuboglu@outlook.com>
2021-06-09 07:10:19 +0300

    Limb

---
+++ Assets/Scripts/limb/Drag.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private Vector3 mOffset, StartPos;
+    private float zCord;
+    public GameObject Elbow, Shoudler;
+
+    [SerializeField] private Transform r1, r2;
+    private Vector3 lastPos;
+    private Vector3 r2FirstPos;
+    private float r;
+
+    [SerializeField] private bool isExceed;
+
+    private Vector2 pointA, pointB, checkMousePosition;
+
+    private void Start()
+    {
+        StartPos = transform.position;
+        r2FirstPos = r2.position;
+        r = Vector3.Distance(r2.position, r1.position);
+    }
+    private void OnMouseDown()
+    {
+        zCord = Camera.main.WorldToScreenPoint(transform.position).z;
+        mOffset = transform.position - GetMouseWorldPos();
+
+        GetComponent<Rigidbody>().isKinematic = false;
+        Elbow.GetComponent<Rigidbody>().isKinematic = false;
+        Shoudler.GetComponent<Rigidbody>().isKinematic = false;
+
+        pointA = Input.mousePosition;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 MousePoint = Input.mousePosition;
+        MousePoint.z = zCord;
+        return Camera.main.ScreenToWorldPoint(MousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        //Debug.Log("distance = " + Vector3.Distance(r2.position, r1.position) + "  r = " + r);
+        pointB = Input.mousePosition;
+        if(pointB != checkMousePosition)
+        {
+            if (pointB.x - pointA.x > 5 && Elbow.transform.localEulerAngles.z < 90)
+            {
+                Elbow.transform.localEulerAngles += Vector3.forward * 3;
+                checkMousePosition = Input.mousePosition;
+            }
+            else if (pointB.x - pointA.x > -5 && Elbow.transform.localEulerAngles.z > 0)
+            {
+                Elbow.transform.localEulerAngles += Vector3.forward * -3;
+                checkMousePosition = Input.mousePosition;
+            }
+        }
+        
+        if (Vector3.Distance(r2.position, r1.position) > r + 0.2f)
+        {
+            if (!isExceed)
+            {
+                lastPos = transform.position;
+                isExceed = true; 
+            }
+            else if (isExceed)
+            {
+                BackToLastPos();
+            }
+        }
+        else if(!isExceed)
+        {
+            transform.position = GetMouseWorldPos() + mOffset;
+        }
+    }
+    private void OnMouseUp()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        Elbow.GetComponent<Rigidbody>().isKinematic = true;
+        Shoudler.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if (isExceed)
+        {
+            BackToLastPos();
+        }
+    }
+    private void BackToLastPos()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, lastPos, Time.deltaTime * 50f);
+        if(transform.position == lastPos)
+        {
+            transform.position = lastPos;
+            isExceed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/limb/testmanager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmanager : MonoBehaviour
+{
+    GameObject gameObject;
+    void Start()
+    {
+        //gameObject = GameObject.FindGameObjectWithTag("Image");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.LookAt(Camera.main.transform);
+    }
+}
